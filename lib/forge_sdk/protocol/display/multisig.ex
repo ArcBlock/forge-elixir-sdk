54c2a8c7406fdc19a647468f09b101d400427593
Tyr Chen <tyr.chen@gmail.com>
2019-03-05 10:03:36 -0800

    do not parse data field for multisig

--- lib/forge_sdk/protocol/display/multisig.ex
+++ lib/forge_sdk/protocol/display/multisig.ex
@@ -6,7 +6,6 @@ defimpl ForgeSdk.Display, for: ForgeAbi.Multisig do
   def display(%{signer: addr, signature: sig, data: data}, _expand? \\ false) do
     addr = if String.valid?(addr), do: addr, else: Base.url_encode64(addr, padding: false)
     sig = Base.url_encode64(sig, padding: false)
-    data = ForgeSdk.display(data)
 
     %{
       signer: addr,

79ec81b8bd36e028a7455316bafa6fd89f5401dc
Peiling Ding <dingpl716@gmail.com>
2019-05-02 10:35:12 -0700

    Use address to query tether state.

--- mix.exs
+++ mix.exs
@@ -41,10 +41,10 @@ defmodule ForgeSdk.MixProject do
 
       # forge family dependencies
       {:mcrypto, "~> 0.2"},
-      {:abt_did_elixir, git: "git@github.com:arcblock/abt-did-elixir.git"},
-      # {:abt_did_elixir, path: "../abt-did-elixir"},
-      {:forge_abi, git: "git@github.com:arcblock/forge-abi.git"},
-      # {:forge_abi, path: "../forge-abi"},
+      # {:abt_did_elixir, git: "git@github.com:arcblock/abt-did-elixir.git"},
+      {:abt_did_elixir, path: "../abt-did-elixir"},
+      # {:forge_abi, git: "git@github.com:arcblock/forge-abi.git"},
+      {:forge_abi, path: "../forge-abi"},
 
       # dev and test
       {:credo, "~> 1.0.0", only: [:dev, :test]},

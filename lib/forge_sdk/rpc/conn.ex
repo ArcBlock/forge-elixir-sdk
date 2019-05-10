6b9c8146316a0f3e3faaf6a65b3a6ae1c9149c4e
Boshan Sun <sunboshan@gmail.com>
2019-05-10 15:12:07 -0700

    Turn off retry logic on gun

--- lib/forge_sdk/rpc/conn.ex
+++ lib/forge_sdk/rpc/conn.ex
@@ -22,10 +22,23 @@ defmodule ForgeSdk.Rpc.Conn do
   @spec close :: any()
   def close, do: Connection.call(__MODULE__, :close)
 
+  @spec child_spec(String.t()) :: map
   def child_spec(addr) do
     %{
       id: __MODULE__,
-      start: {__MODULE__, :start_link, [addr, []]},
+      start:
+        {__MODULE__, :start_link,
+         [
+           addr,
+           [
+             adapter_opts: %{
+               # turn off keepalive to prevent zombie connections for grpc server
+               http2_opts: %{keepalive: :infinity},
+               # disable retry on gun's part to prevent undesired zombie connections
+               retry: 0
+             }
+           ]
+         ]},
       type: :worker,
       restart: :permanent,
       shutdown: 500
@@ -39,11 +52,11 @@ defmodule ForgeSdk.Rpc.Conn do
   def init({"tcp://" <> addr, opts}), do: {:connect, :init, %{addr: addr, opts: opts, chan: nil}}
 
   def connect(_, %{chan: nil, addr: addr, opts: opts} = state) do
-    Logger.info("Forge ABI RPC: reconnect to #{addr}")
+    Logger.info("Forge ABI RPC: reconnect to #{addr}...")
 
     case Client.connect(addr, opts) do
       {:ok, chan} -> {:ok, %{state | chan: chan}}
-      {:error, _} -> {:backoff, 500, state}
+      {:error, _} -> {:backoff, 5000, state}
     end
   end
 

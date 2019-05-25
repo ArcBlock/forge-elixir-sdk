498e6923c2f847ab01c0cc20492e84055e5b9c41
Boshan Sun <sunboshan@gmail.com>
2019-05-24 23:45:05 -0700

    Fix display issue for enum type for forge_web

--- lib/forge_sdk/protocol/display/wallet.ex
+++ lib/forge_sdk/protocol/display/wallet.ex
@@ -5,8 +5,18 @@ defimpl ForgeSdk.Display, for: ForgeAbi.WalletType do
 
   def display(%{pk: pk, hash: hash, address: address, role: role} = type, expand? \\ false) do
     case expand? do
-      true -> "#(#{pk}, #{hash}, #{address}, #{role})"
-      _ -> type
+      true ->
+        "#(#{pk}, #{hash}, #{address}, #{role})"
+
+      _ ->
+        basic = Map.from_struct(type)
+
+        Map.merge(basic, %{
+          address: ForgeAbi.EncodingType.value(address),
+          pk: ForgeAbi.KeyType.value(pk),
+          hash: ForgeAbi.HashType.value(hash),
+          role: ForgeAbi.RoleType.value(role)
+        })
     end
   end
 end
